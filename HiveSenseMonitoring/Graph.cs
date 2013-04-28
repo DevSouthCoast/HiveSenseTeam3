a2b0c552320c5e507f31c31e0554435b5476be35
dhendo <dhendo@gmail.com>
2013-04-28 12:30:50 +0100

    Added MVC App

---
+++ HiveSenseMonitoring/Graph.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HiveSenseMonitoring
+{
+    public class Graph
+    {
+
+
+        public Graph(string Title, string URL, string id)
+        {
+            // TODO: Complete member initialization
+            this.Title = Title;
+            this.URL = URL;
+            this.id = id;
+        }
+        public String Title { get; set; }
+        public String URL { get; set; }
+
+        public string id { get; set; }
+    }
+}
